b0ffb7561774d5831cea088e980ff7cdfa6eac91
ivela064 <MY_NAME@example.com>
2023-10-30 15:20:43 -0400

    Update FE for fetching db data

--- src/screens/ContactAdmin.js
+++ src/screens/ContactAdmin.js
@@ -4,90 +4,72 @@ import { fetchContactUs } from '../redux/actions/userActions';
 import { fetchUsers, fetchPatientRegistration } from '../redux/actions/userActions';
 import '../styles/screens/ContactAdmin.css';
 import { Checkbox } from '@mui/material';
+import axios from 'axios'
 
 
 function ContactAdmin() {
-  const dispatch = useDispatch();
-  const contactUsData = useSelector((state) => state.user.contactUsData);
-  const [showTable, setShowTable] = useState(false);
+  // const dispatch = useDispatch();
+  // const contactUsData = useSelector((state) => state.user.contactUsData);
+  // const [showTable, setShowTable] = useState(false);
 
-  const handleFetchContactUs = () => {
-    // Clear the table and show loading state
-    setShowTable(false);
+  const [records, setRecords] = useState([])
+  useEffect(()=> {
+    //axios.get("https://jsonplaceholder.typicode.com/users")
+    axios.get("http://localhost:8080/api/users/contact")
+    .then(res => {setRecords(res.data)})
+    .catch(err => console.log(err))
+  }, [])
+  
+  // const handleFetchContactUs = () => {
+  //   // Clear the table and show loading state
+  //   setShowTable(false);
 
-    dispatch(fetchContactUs());
-  };
+  //   dispatch(fetchContactUs());
+  // };
 
 
   return (
-    <div className="ContactAdmin">
-      <div className='container'>
-        <button type="refreshContactAdmin" onClick={handleFetchContactUs}>
-          Refresh
-        </button>
-        <br></br>
 
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>Topic</th>
+                <th>Message</th>
+                <th>Time</th>
+                <th>Reply</th>
 
-        <h4>
-          <table border={2}>
-            <tr>
-              <th>Contact No.</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-              <th>Topic</th>
-              <th>Message</th>
-              <th>Submit Time</th>
-              <th>Reply Status</th>
-            </tr>
-
-            <tr>
-              <td> 123</td>
-              <td> Shuli</td>
-              <td> swang515@uottawa.ca</td>
-              <td> 3435585817</td>
-              <td> Doctor Replated Queries</td>
-              <td> Is Doctor White available on Oct 19?</td>
-              <td> 2023-10-26 14:58:15</td>
-              <td> <Checkbox></Checkbox> </td>
-            </tr>
-
-
-            {/*
+                {/* <th>contact_topic</th>
+                <th>contact_message</th>
+                <th>contact_time</th>
+                <th>contact_reply</th> */}
+              </tr>
+              </thead>
+              <tbody>
+                {records.map((r,i)=> {
+                  return <tr key={i}>
+                    <td> {r.id}</td>
+                    <td> {r.contact_name}</td>
+                    <td> {r.contact_phone}</td>
+                    <td> {r.contact_email}</td>
+                    <td> {r.contact_topic}</td>
+                    <td> {r.contact_message}</td>
+                    <td> {r.contact_time}</td>
+                    <td> {r.contact_reply}</td>
+                    {/* <td> {r.topic}</td>
+                    <td> {r.message}</td>
+                    <td> {r.time}</td>
+                    <td> {r.reply}</td> */}
+                  </tr>
+                })}
+              </tbody>
             
-            I dont know why this dont work
-            
-
-
-            
-            {contactUsData.length > 0 && (
-              <div>
-                {contactUsData.map((contact) => (
-                  <li key={contact.id}>
-                    <td> id: {contact.id}</td>
-                    <td> sd{contact.contact_name}</td>
-                    <td> {contact.contact_email}</td>
-                    <td> {contact.contact_phone}</td>
-                    <td> {contact.contact_topic}</td>
-                    <td> {contact.contact_message}</td>
-                    <td> {contact.contact_time}</td>
-                    <td> {contact.contact_reply}</td>
-                  </li>
-                ))}
-              </div>
-            )}
-            
-            
-            
-            */}
-
-          
-
           </table>
-        </h4>
-      </div>
-    </div>
-
+        </div>
 
 
   );
